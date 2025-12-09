4ccd2fbb3d2bb9960a4a63daec17ec15a3371c94
Rafaela Soares <rsoaresd@redhat.com>
2025-12-09 11:09:20 +0000

    requested changes

--- make/podman.mk
+++ make/podman.mk
@@ -3,14 +3,13 @@ TARGET_REGISTRY := quay.io
 IMAGE_TAG ?= ${GIT_COMMIT_ID_SHORT}
 # sandbox-rhdh-plugin provisory
 IMAGE ?= ${TARGET_REGISTRY}/${QUAY_NAMESPACE}/sandbox-rhdh-plugin:${IMAGE_TAG}
-QUAY_USERNAME ?= ${QUAY_NAMESPACE}
 IMAGE_PLATFORM ?= linux/amd64
 
 .PHONY: podman-push
 podman-push:
-	rm -rf plugins/sandbox/dist-dynamic && \
-	rm -rf red-hat-developer-hub-backstage-plugin-sandbox && \
-	yarn install && \
+	rm -rf plugins/sandbox/dist-dynamic
+	rm -rf red-hat-developer-hub-backstage-plugin-sandbox
+	yarn install
 	npx --yes @janus-idp/cli@latest package package-dynamic-plugins \
 	  --tag $(IMAGE) \
 	  --platform $(IMAGE_PLATFORM)
